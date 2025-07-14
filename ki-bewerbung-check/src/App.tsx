e7fd0336c94f449a54098ad045194e09bb996845
Hannes Hering <hanneshering1999@gmail.com>
2025-07-14 21:37:20 +0200

    landing page

--- ki-bewerbung-check/src/App.tsx
+++ ki-bewerbung-check/src/App.tsx
@@ -1,41 +1,46 @@
 import "./App.css";
 import ThemeSwitch from "../components/ThemeSwitch";
 import Sidebar from "../components/Sidebar";
-import Evaluation from "../components/Evaluation";
+import Welcome from "../pages/Welcome";
+import Experiences from "../pages/Experiences";
 import PersonalInformation from "../pages/PersonalInformation";
-import Experiences from "../pages/Experiences"
 import Skills from "../pages/Skills";
-import { Routes, Route } from "react-router-dom";
-import { library } from '@fortawesome/fontawesome-svg-core'
+import Evaluation from "../components/Evaluation"; // Importiere Evaluation-Komponente
+import { library } from '@fortawesome/fontawesome-svg-core';
 import { faFaceSmile, faFaceMeh, faFaceFrown } from "@fortawesome/free-solid-svg-icons";
-import AppProvider from "../context/AppProvider"
-
+import AppProvider from "../context/AppProvider";
+import { Routes, Route, Link, useLocation } from "react-router-dom";
 
 function App() {
+  library.add(faFaceSmile, faFaceMeh, faFaceFrown);
 
-  library.add(faFaceSmile, faFaceMeh, faFaceFrown)
-  
+  const location = useLocation();
+  const isRootPath = location.pathname === "/";
+
   return (
     <AppProvider>
       <div className="min-h-screen min-w-screen overscroll-x-none">
         <div className="navbar fixed top-0 text-white bg-accent shadow-sm justify-between z-999">
-          <a className="btn btn-ghost text-3xl">EVOKE</a>
+          <Link to="/" className="btn btn-ghost text-3xl">EVOKE</Link>
           <ThemeSwitch />
         </div>
         <div className="flex flex-row">
-          <Sidebar />
+          {!isRootPath && <Sidebar />}
           <div className="flex-1">
-            {/* Routes */}
             <Routes>
-              <Route path="/" element={<Experiences />} />
+              <Route path="/" element={<Welcome />} />
+              <Route
+                path="/experiences"
+                element={<Experiences />}
+              />
               <Route path="/personal-information" element={<PersonalInformation />} />
               <Route path="/skills" element={<Skills />} />
             </Routes>
           </div>
-          <Evaluation />
+          {!isRootPath && <Evaluation />}
         </div>
       </div>
-    </ AppProvider>
+    </AppProvider>
   );
 }
 
